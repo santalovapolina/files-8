338461657ab952b437941004ea597e177f149493
santalovapolina <polinasantalova@yandex.ru>
2023-01-22 15:48:22 +0700

    added zipParseTest

---
+++ src/test/java/guru/qa/ParsePractice.java
@@ -0,0 +1,52 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ParsePractice {
+
+    ClassLoader classLoader = ParsePractice.class.getClassLoader();
+
+    @Test
+    void zipParseTest() throws Exception {
+        try (
+                InputStream resource = classLoader.getResourceAsStream("homework.zip");
+                ZipInputStream zis = new ZipInputStream(resource)
+        ) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+
+                if (entry.getName().contains("xlsx")) {
+                    XLS content = new XLS(zis);
+                    assertThat(content.excel.getSheetAt(0).getRow(5).getCell(0).getStringCellValue()).contains("junio");
+                } else if (entry.getName().contains("pdf")) {
+                    PDF content = new PDF(zis);
+                    assertThat(content.text).contains("REMEMBER THESE SHORTCUTS");
+                } else if (entry.getName().contains("csv")) {
+                    CSVReader reader = new CSVReader(new InputStreamReader(zis));
+                    List<String[]> content = reader.readAll();
+                    assertThat(content.get(2)[0]).contains("march");
+                }
+            }
+        }
+    }
+
+
+    @Test
+    void jsonParseTest() {
+
+
+    }
+}
+
+
