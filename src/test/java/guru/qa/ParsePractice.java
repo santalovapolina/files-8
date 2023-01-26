c5ebb5e13d4d18df54b37927f32b8df21fa0f1eb
santalovapolina <polinasantalova@yandex.ru>
2023-01-26 14:15:52 +0700

    added zip file

--- src/test/java/guru/qa/ParsePractice.java
+++ src/test/java/guru/qa/ParsePractice.java
@@ -27,16 +27,16 @@ public class ParsePractice {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
 
-                if (entry.getName().contains("xlsx")) {
+                if (entry.getName().contains("xls")) {
                     XLS content = new XLS(zis);
-                    assertThat(content.excel.getSheetAt(0).getRow(5).getCell(0).getStringCellValue()).contains("junio");
+                    assertThat(content.excel.getSheetAt(0).getRow(5).getCell(1).getStringCellValue()).contains("Junio");
                 } else if (entry.getName().contains("pdf")) {
                     PDF content = new PDF(zis);
                     assertThat(content.text).contains("REMEMBER THESE SHORTCUTS");
                 } else if (entry.getName().contains("csv")) {
                     CSVReader reader = new CSVReader(new InputStreamReader(zis));
                     List<String[]> content = reader.readAll();
-                    assertThat(content.get(2)[0]).contains("march");
+                    assertThat(content.get(2)[1]).contains("Marzo");
                 }
             }
         }
